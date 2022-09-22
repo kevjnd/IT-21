4df6e624d6f07c5f4c2952f1d11ca34abd3c9115
KevJn <94998639+0Sh0t@users.noreply.github.com>
2022-09-22 13:32:21 +0300

    Add files via upload

--- JavaH3.java
+++ JavaH3.java
@@ -3,49 +3,62 @@ public class JavaH3 {
  
 //Ühemõõtmeline massiiv arvudest
 
- public static void main(String[] args) {
- 
- int[][] numbrid = new int[1][1];
- 
- 
- numbrid[0][0] = 12;
- numbrid[0][0] = 5;
- numbrid[0][0] = 10;
- numbrid[0][0] = 10;
- numbrid[0][0] = 28;
- numbrid[0][0] = 17;
- numbrid[0][0] = 5;
- numbrid[0][0] = 19;
- numbrid[0][0] = 0;
- numbrid[0][0] = 13;
- numbrid[0][0] = 0;
- numbrid[0][0] = 22;
- numbrid[0][0] = 7;
- numbrid[0][0] = 0;
- numbrid[0][0] = 17;
- numbrid[0][0] = 2;
- numbrid[0][0] = 24;
- numbrid[0][0] = 1;
- numbrid[0][0] = 13;
- numbrid[0][0] = 29;
- numbrid[0][0] = 0;
- numbrid[0][0] = 7;
- numbrid[0][0] = 16;
- numbrid[0][0] = 8;
- numbrid[0][0] = 7;
- numbrid[0][0] = 4;
- numbrid[0][0] = 27;
- numbrid[0][0] = 8;
- numbrid[0][0] = 8;
- numbrid[0][0] = 23;
- numbrid[0][0] = 26;
- numbrid[0][0] = 25;
- numbrid[0][0] = 15;
- numbrid[0][0] = 2;
- numbrid[0][0] = 26;
- numbrid[0][0] = 1;
+    public static void main(String[] args) {
+    
+    int[][] numbrid = new int[1][1];
+    
+    
+    numbrid[0][0] = 12;
+    numbrid[0][0] = 5;
+    numbrid[0][0] = 10;
+    numbrid[0][0] = 10;
+    numbrid[0][0] = 28;
+    numbrid[0][0] = 17;
+    numbrid[0][0] = 5;
+    numbrid[0][0] = 19;
+    numbrid[0][0] = 0;
+    numbrid[0][0] = 13;
+    numbrid[0][0] = 0;
+    numbrid[0][0] = 22;
+    numbrid[0][0] = 7;
+    numbrid[0][0] = 0;
+    numbrid[0][0] = 17;
+    numbrid[0][0] = 2;
+    numbrid[0][0] = 24;
+    numbrid[0][0] = 1;
+    numbrid[0][0] = 13;
+    numbrid[0][0] = 29;
+    numbrid[0][0] = 0;
+    numbrid[0][0] = 7;
+    numbrid[0][0] = 16;
+    numbrid[0][0] = 8;
+    numbrid[0][0] = 7;
+    numbrid[0][0] = 4;
+    numbrid[0][0] = 27;
+    numbrid[0][0] = 8;
+    numbrid[0][0] = 8;
+    numbrid[0][0] = 23;
+    numbrid[0][0] = 26;
+    numbrid[0][0] = 25;
+    numbrid[0][0] = 15;
+    numbrid[0][0] = 2;
+    numbrid[0][0] = 26;
+    numbrid[0][0] = 1;
 
- System.out.println(numbrid[0][0]);
- }
- 
+    System.out.println(numbrid[0][0]);
+    }
+    
+
+//Mitmemõõtmeline massiiv arvudest
+    public static void mitmemootmeline(String[] args) {
+
+        int numbrid2 = {{1,23},{2,15},{3,29},{4,15},{5,26},{6,17},{7,26},{8,15},{9,28},{10,12},{11,24},{12,16},{13,21},{14,10},{15,10},{16,26},{17,27},{18,19},{19,14},{20,14},{21,14},{22,26},{23,20},{24,28},{25,29},{26,11},{27,28},{28,19},{29,25},{30,12}};
+
+        System.out.println(numbrid2);
+    }
+
+//Väljastab esimese elemendi väärtuse
+    
+    
+
 }
