3ba1e27b8f2f47b4288114b527b8b4e2c7291865
KevJn <94998639+0Sh0t@users.noreply.github.com>
2022-09-22 13:04:52 +0300

    Add files via upload

--- JavaH3.java
+++ JavaH3.java
@@ -1,12 +1,51 @@
+//Harjutus 3
 public class JavaH3 {
  
+//Ühemõõtmeline massiiv arvudest
+
  public static void main(String[] args) {
  
- int[][] numbrid = new int[4][3];
+ int[][] numbrid = new int[1][1];
  
- numbrid[1][2] = (12,5,10,10,28,17,5,19,0,13,0,22,7,0,17,2,24,1,13,29,0,7,16,8,7,4,27,8,8,23,26,25,15,2,26,1);
+ 
+ numbrid[0][0] = 12;
+ numbrid[0][0] = 5;
+ numbrid[0][0] = 10;
+ numbrid[0][0] = 10;
+ numbrid[0][0] = 28;
+ numbrid[0][0] = 17;
+ numbrid[0][0] = 5;
+ numbrid[0][0] = 19;
+ numbrid[0][0] = 0;
+ numbrid[0][0] = 13;
+ numbrid[0][0] = 0;
+ numbrid[0][0] = 22;
+ numbrid[0][0] = 7;
+ numbrid[0][0] = 0;
+ numbrid[0][0] = 17;
+ numbrid[0][0] = 2;
+ numbrid[0][0] = 24;
+ numbrid[0][0] = 1;
+ numbrid[0][0] = 13;
+ numbrid[0][0] = 29;
+ numbrid[0][0] = 0;
+ numbrid[0][0] = 7;
+ numbrid[0][0] = 16;
+ numbrid[0][0] = 8;
+ numbrid[0][0] = 7;
+ numbrid[0][0] = 4;
+ numbrid[0][0] = 27;
+ numbrid[0][0] = 8;
+ numbrid[0][0] = 8;
+ numbrid[0][0] = 23;
+ numbrid[0][0] = 26;
+ numbrid[0][0] = 25;
+ numbrid[0][0] = 15;
+ numbrid[0][0] = 2;
+ numbrid[0][0] = 26;
+ numbrid[0][0] = 1;
 
- System.out.println(numbrid[1][2]);
+ System.out.println(numbrid[0][0]);
  }
  
 }
