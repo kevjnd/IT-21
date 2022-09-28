8284f7a6151cf8b67adbdeb42ec6b5989e4a6381
KevJn <94998639+0Sh0t@users.noreply.github.com>
2022-09-28 23:13:32 +0300

    Add files via upload

--- Java iseseisvad ül/Java Iseseisev ül 17.java
+++ Java iseseisvad ül/Java Iseseisev ül 17.java
@@ -1,18 +1,17 @@
-//Iseseisev ülesanne 17
+import java.util.Scanner;
 
-class Iseseisev17  
-{  
-public static void main(String args[])   
-{  
-int number=100;  
-System.out.print("Paarisarvud: ");  
-for (int i=1; i<=number; i++)   
-{  
-//Kontrollib kas arv on paarisarv
-if (i%2==0)   
-{  
-System.out.print(i + " ");  
-}  
-}  
-}  
-}  
+class EvenOdd {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Sisestage number: ");
+        int num = reader.nextInt();
+
+        if(num % 2 == 0)
+            System.out.println(num + " Paarisarv");
+        else
+            System.out.println(num + " Paarituarv");
+    }
+}
