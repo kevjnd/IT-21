56a85dc95d8f910edeb52c8d92fb5f2738ccfd01
KevJn <94998639+0Sh0t@users.noreply.github.com>
2022-09-28 23:22:26 +0300

    Update Java Iseseisev ül 17.java

--- Java iseseisvad ül/Java Iseseisev ül 17.java
+++ Java iseseisvad ül/Java Iseseisev ül 17.java
@@ -1,13 +1,12 @@
 //Iseseisev ülesanne 17
 
-class OddEvenInArrayExample{  
+class Paarisarvud{  
     public static void main(String args[]){  
     int a[]={3,5,4,12,5};  
   
-    System.out.println("Paarisarvud:");  
     for(int i=0;i<a.length;i++){  
     if(a[i]%2==0){  
-    System.out.println(a[i]);  
+    System.out.println("Paarisarv: " + a[i]);  
     }  
     }  
     }}  
