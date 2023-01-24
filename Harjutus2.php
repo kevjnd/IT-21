9efb82c65ba20f0b964e38f0cbf7796eaa4cb7c6
KevJn <94998639+0Sh0t@users.noreply.github.com>
2023-01-24 10:05:09 +0200

    Add files via upload

---
+++ Harjutus2.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Harjutus2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container"> 
+    <h1>Harjutus2</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  
+
+    <?php
+        //Harjutus2'
+        //Kevin Joarand
+        //24.01.2023'
+
+        $x = 8;
+        $y = 2;
+        $liitm = $x + $y;
+        $lahut = $x - $y;
+        $korru = $x * $y;
+        $jagam = $x / $y;
+        $jaak = $x % $y;
+        echo "Vastused:<br> $liitm<br> $lahut<br> $korru<br> $jagam<br> $jaak";
+    
+    
+    ?>
+</div>
+</body>
